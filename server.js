96fce370a2298d2fcef75265a98f3807f36492f1
tobi9310 <tobias22.jena@web.de>
2025-04-10 19:56:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
